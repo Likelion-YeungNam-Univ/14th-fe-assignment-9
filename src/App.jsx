c5d2a4fdf13f55bef3d6615eb16d0fad8bba1043
copepb <qwwo5015@gmail.com>
2026-05-21 13:12:50 +0900

    테마 useState 설정 및 Provider 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from './container'
 import Header from './header'
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
 
+  const [isDark, setIsDark] = useState(false);
+
+  const changeTheme = () => {
+    setIsDark((e) => !e)
+  }
+
   return (
+    <ThemeContext.Provider value={{isDark, changeTheme}}>
+
     <div className="min-h-screen bg-[#ffffff]">
       <Header />
       <Container />
     </div>
+
+    </ThemeContext.Provider>
   )
 }
 
