ca191df18378a35259adb85e73f92f25f21ffe9c
knayoung0 <kzeron09@naver.com>
2026-05-24 11:02:12 +0900

    App.jsx에 Provider 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Story from './Story'
 import Post from './Post'
 import Head from './Head'
 import Message from './Message'
-import LoginContext from './loginContext'
+import LoginContext from './LoginContext'
 
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
-    <>
-      <Head/>
-      <Story/>
-      <Post/>
-      <Message/>
-    </>
+    <LoginContext.Provider value={{isLogin, setIsLogin}}>
+      {isLogin ? (
+        <>
+          <Head/>
+          <Story/>
+          <Post/>
+          <Message/>
+        </>
+      ) : (
+        <LoginPage/>
+      )}
+    </LoginContext.Provider>
   )
 }
 
