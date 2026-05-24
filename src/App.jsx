180e0c736e40226f465752f64e2003592032dbc1
knayoung0 <kzeron09@naver.com>
2026-05-24 10:52:19 +0900

    LoginContext 생성 및 Logout 버튼 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Story from './Story'
 import Post from './Post'
 import Head from './Head'
 import Message from './Message'
+import LoginContext from './loginContext'
 
 
 const App = () => {
--- src/Head.jsx
+++ src/Head.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Head = () => {
     return (
         <>
-            <div className="p-4 w-30 h-20 flex justify-between">
+            <div className="p-4 w-30 h-20 flex justify-between items-center">
                 <svg  xmlns="http://www.w3.org/2000/svg" width={30} height={60} 
                 fill={"currentColor"} viewBox={"0 0 24 24"}>
                 <path d="M11 17v4h2v-8h8v-2h-8V3h-2v8H3v2h8v4"/>
@@ -11,7 +11,8 @@ const Head = () => {
 
                 <div className= "text-3xl">Instagram</div>
                 
-                <div className="flex space-x-2">
+                <div className="flex space-x-2 items-center">
+                    <button className="border bg-stone-300 hover:bg-stone-400 text-white rounded-xl w-20 h-10">Logout</button>
                     <svg  xmlns="http://www.w3.org/2000/svg" width={30} height={60} 
                     fill={"currentColor"} viewBox={"0 0 24 24"}>
                     <path d="M13.12 12.76 12 10.5l-1.12 2.26-2.38.21 1.85 1.85-.82 2.68L12 15.85l2.47 1.65-.82-2.68 1.85-1.85z"/><path d="m19.94 7.68-.03-.09a.8.8 0 0 0-.2-.29l-5-5a1 1 0 0 0-.3-.2l-.09-.03a.9.9 0 0 0-.27-.05c-.02 0-.04-.01-.05-.01H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-12s-.01-.04-.01-.06c0-.09-.02-.17-.05-.26ZM6 20V4h7v4c0 .55.45 1 1 1h4v11z"/>
