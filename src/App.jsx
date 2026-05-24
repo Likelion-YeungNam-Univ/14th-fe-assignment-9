27e02e7841a9d69f835e4d89c2a188fc60076602
SeEun <oseeun60@gmail.com>
2026-05-24 21:28:56 +0900

    Provider 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WebtoonPage from './pages/WebtoonPage'
+import ThemeContext from './ThemeContext';
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
   return (
-    <div><WebtoonPage /></div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div><WebtoonPage /></div>
+    </ThemeContext.Provider>
   )
 }
 
