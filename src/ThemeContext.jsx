fb7b7519039542de577702a0a1f529e3959102dd
Park jin-a <jiiinap@naver.com>
2026-05-25 00:09:40 +0900

    프론트엔드 9회차 과제 - 박진아

---
+++ src/ThemeContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const ThemeContext = createContext();
+
+// Provider 컴포넌트 생성
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// 다른 컴포넌트에서 쉽게 쓰기 위해서!!
+export const useTheme = () => useContext(ThemeContext);
