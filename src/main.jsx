3ebc1e6fd52d08765f74f7ba3d556c5d05957736
Park jin-a <jiiinap@naver.com>
2026-05-25 22:57:06 +0900

    chore: main.jsx에 ThemeProvider 적용

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {ThemeProvider} from './ThemeContext.jsx' //themecontext 추가
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider> {/* 앱 감싸기: 타메 적용을 어플 전체에 공유하기 위해서! */}
     <App />
+    </ThemeProvider>
   </StrictMode>,
 
 )
