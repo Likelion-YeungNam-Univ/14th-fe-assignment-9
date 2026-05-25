0f6e58d83a3b4fe906c60f4fa9637ca58b423c87
Park jin-a <jiiinap@naver.com>
2026-05-25 23:54:31 +0900

    feat: laguageprovider; 전역 언어 상태 적용을 위한 main.jsx Provider 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {ThemeProvider} from './ThemeContext.jsx' //themecontext 추가
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { ThemeProvider } from './ThemeContext.jsx'; // themecontext 추가
+import { LanguageProvider } from './contexts/LanguageContext.jsx'; // languagecontext 추가
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider> {/* 앱 감싸기: 타메 적용을 어플 전체에 공유하기 위해서! */}
-    <App />
+    {/* 다크/화이트 모드, 안에 번역 모드 감싸기 */}
+    <ThemeProvider>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </ThemeProvider>
   </StrictMode>,
-
 )
